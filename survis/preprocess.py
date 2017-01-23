6d6a846ff2159bca7a3d4572c21b763fed52a7a1
Josh Borrow <josh.borrow@gmail.com>
2017-01-23 15:25:16 +0000

    changed to an oo approach

--- survis/preprocess.py
+++ survis/preprocess.py
@@ -5,35 +5,50 @@ import numpy as np
 
 def test():
     fname = "test_data.hdf5"
-    head, gas, star = read_data(fname)
+    DG = DataGridder(fname, 1, 2, 3, 4, 5, 6)
+    head, gas, star = DG.read_data()
 
     print([x for x in star.keys()])
     print(gas['Density'][0])
     return
 
 
-def read_data(fname):
-    # Breaks down the data into particle types
-    f = h5py.File(fname, 'r')
+class DataGridder(object):
+    def __init__(self, fname, binsx, binsy, xmin, xmax, ymin, ymax):
+        self.fname = fname
 
-    # Header, gas, stars
-    return f['Header'], f['PartType0'], f['PartType4']
+        self.binsx = binsx
+        self.binsy = binsy
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
 
-
-def bin_data(raw_data, binsx, binsy, xmin, xmax, ymin, ymax):
-    # raw_data is e.g. GADGET['PartType0'].
-    # grids are left as flat lists for efficiency
-    id_grid = [[]*(binsx*binsy)]
-    mass_arr = np.zeros(binsx*binsy)
-
-    n_particles = len(raw_data['Coordinates'])
-
-    coords = raw_data['Coordinates']
-    dens = raw_data['Density']
+        return
 
 
-    for particle in range(n_particles):
-        binx = np.floor(coords[particle])
+    def read_data(self):
+        # Breaks down the data into particle types
+        f = h5py.File(self.fname, 'r')
+
+        # Header, gas, stars
+        return f['Header'], f['PartType0'], f['PartType4']
 
 
-    return
+    def bin_data(self, raw_data):
+        # raw_data is e.g. GADGET['PartType0'].
+        # grids are left as flat lists for efficiency
+        self.id_grid = [[]*(self.binsx*self.binsy)]
+        self.mass_arr = np.zeros(self.binsx*self.binsy)
+
+        n_particles = len(raw_data['Coordinates'])
+
+        coords = raw_data['Coordinates']
+        dens = raw_data['Density']
+
+
+        for particle in range(n_particles):
+            binx = np.floor(coords[particle])
+
+
+        return
