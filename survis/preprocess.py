eb1ffaf1c8e15f4fbd2e94f848e06827fc7431b3
Josh Borrow <josh.borrow@gmail.com>
2017-01-24 14:12:04 +0000

    migrated tests to central file

--- survis/preprocess.py
+++ survis/preprocess.py
@@ -6,40 +6,6 @@
 import h5py
 import numpy as np
 
-def test():
-    # Plots a few things with the example data
-
-    import matplotlib.pyplot as plt
-
-    fname = "test_data.hdf5"
-    res = 50
-    DG = DataGridder(fname, res, res, -100, 100, -100, 100)
-
-    print("Star Particle Attrs: {}".format([x for x in DG.star.keys()]))
-    print("Gass Mass: {}; Star Mass: {}".format(DG.gas_mass, DG.star_mass))
-
-    # Now we vis!
-    f = plt.figure(1)
-    gas_sd = np.reshape(DG.gas_mass_arr, (res, res))
-    plt.imshow(gas_sd)
-    plt.title('Gas mass')
-    f.show()
-
-    g = plt.figure(2)
-    gas_v = np.reshape(DG.mean_gas_vel_arr, (res, res))
-    plt.imshow(gas_v)
-    plt.title('Gas velocity')
-    g.show()
-
-    h = plt.figure(3)
-    gas_d = np.reshape(DG.mean_gas_d_arr, (res, res))
-    plt.imshow(gas_d)
-    plt.title('Gas density')
-    h.show()
-    input()  # keep figures alive
-
-    return
-
 
 class DataGridder(object):
     def __init__(self, fname, binsx, binsy, xmin, xmax, ymin, ymax):
--- tests.py
+++ tests.py
@@ -1,4 +1,35 @@
 import survis.preprocess as pre
+import matplotlib.pyplot as plt
+import numpy as np
 
 print("Running preprocess.py tests")
-pre.test()
+# Plots a few things with the example data
+
+fname = "test_data.hdf5"
+res = 50
+DG = pre.DataGridder(fname, res, res, -100, 100, -100, 100)
+
+print("Star Particle Attrs: {}".format([x for x in DG.star.keys()]))
+print("Gass Mass: {}; Star Mass: {}".format(DG.gas_mass, DG.star_mass))
+
+# Now we vis!
+f = plt.figure(1)
+gas_sd = np.reshape(DG.gas_mass_arr, (res, res))
+plt.imshow(gas_sd)
+plt.title('Gas mass')
+f.show()
+
+g = plt.figure(2)
+gas_v = np.reshape(DG.mean_gas_vel_arr, (res, res))
+plt.imshow(gas_v)
+plt.title('Gas velocity')
+g.show()
+
+h = plt.figure(3)
+gas_d = np.reshape(DG.mean_gas_d_arr, (res, res))
+plt.imshow(gas_d)
+plt.title('Gas density')
+h.show()
+input()  # keep figures alive
+
+
