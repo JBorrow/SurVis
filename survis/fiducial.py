0489a371b7d0b20fa2b6ac0c8949fb3900112e24
Josh Borrow <josh.borrow@gmail.com>
2017-02-22 09:18:38 +0000

    added surface density within an annulus calculation

---
+++ survis/fiducial.py
@@ -0,0 +1,29 @@
+""" Contains extraction functions for things at a fiducial radius over time """
+
+import numpy as np
+
+def surface_density(DG, R, dR):
+    """ Takes the DataGrid, some radius R to find the surface density at over
+        some smoothing dR (finds particles within R - dR/2 and R + dR/2) """
+
+    def sd_per_type(data, mass):
+        """ Calculates the surface density for a given GADGET dataset and
+            particle mass. """
+        radii = np.sqrt(np.sum(np.square(data['Coordinates'][()]), 1))
+        within_bounds = np.ma.array(radii, mask=((radii > R-dR) and (radii < R+dR)))
+
+        n_particles = np.count(within_bounds)
+        m_particles = n_particles * mass
+
+        area_enclosed = 4 * np.pi * R * dR
+
+        return m_particles / area_enclosed
+
+    sd_gas = sd_per_type(DG.gas, DG.gas_mass)
+    sd_star = sd_per_type(DG.star, DG.star_mass)
+
+    return [sd_gas, sd_star]
+
+
+def toomre_Q():
+    return 0.
