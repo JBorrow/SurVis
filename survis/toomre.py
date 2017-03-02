0a07052207a289f43b2980c69941a2e8cb69e4a3
Josh Borrow <josh.borrow@gmail.com>
2017-03-02 22:20:18 +0000

    forogt to square root sound speed

--- survis/toomre.py
+++ survis/toomre.py
@@ -18,7 +18,7 @@ def sound_speed_sne(density, f=1, F=1, fg=0.1, P=300000, G=4.302e-6):
 
     entropy = 4.5 * (f/F)**(3./2.) * G**(3./4.) * P**(1./2.) * fg **(-1)
 
-    return (5./4.) * entropy * density**(1./4.)
+    return np.sqrt((5./4.) * entropy * density**(1./4.))
 
 
 def Q_gas(sound_speed, kappa, density, surface_density, G=4.302e-6):
@@ -27,8 +27,7 @@ def Q_gas(sound_speed, kappa, density, surface_density, G=4.302e-6):
     c_s[surface_density == 0] = 0
     sd_masked = surface_density + (surface_density == 0)
 
-    # our surface density is given in msun/kpc^2 so we need a conversion factor
-    return 3.086e16 * ((c_s * kappa)/(np.pi * G * sd_masked))
+    return ((c_s * kappa)/(np.pi * G * sd_masked))
 
 
 def Q_star():
