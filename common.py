4bfef5926a30f8d5202390b625eaf2ab6fa71c9c
Josh Borrow <josh.borrow@gmail.com>
2017-03-02 14:15:13 +0000

    added multiple cpu run as script option

--- common.py
+++ common.py
@@ -27,8 +27,10 @@ def processing_run(filename, res, bbox_x, bbox_y):
     data_grid = survis.preprocess.DataGridder(filename,
                                               res[0],
                                               res[1],
-                                              bbox_x,
-                                              bbox_y)
+                                              bbox_x[0],
+                                              bbox_x[1],
+                                              bbox_y[0],
+                                              bbox_y[1])
 
     # First we extract the maps
     Q_map = survis.helper.get_toomre_Q(data_grid,
@@ -46,19 +48,49 @@ def processing_run(filename, res, bbox_x, bbox_y):
                                        smoothing,
                                        survis.toomre.sound_speed_sne)
 
-    return Q_map, sd_map, Q_r, sd_r
+    return Q_map, sd_map, Q_r, sd_r, filename
+
+
+def get_snaps(directory = "."):
+    n_snaps = 0
+
+    while "snapshot_{:03d}.hdf5".format(n_snaps) in os.listdir(directory):
+        n_snaps += 1
+        if n_snaps > 10000:
+            break
+
+    return n_snaps
 
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     # Run in script mode
 
     import sys
     import os
-
     from multiprocessing import Pool
+    from functools import partial
 
-    while "snapshot_{:03d}.hdf5".format(n_snaps) in os.listdir("."):
-        n_snaps += 1
-        if n_snaps > 10000:
-            break
+    if "--save" in sys.argv:
+        import pickle
+
+    # Physics Setup
+    bbox_x = [-100, 100]
+    bbox_y = bbox_x
+    res_elem = 5.
+    res = survis.helper.get_res(res_elem, bbox_x, bbox_y)
+
+    # Computing Setup
+    n_cpus = os.cpu_count() if os.cpu_count() <= 8 else 8
+
+    n_snaps = get_snaps()
+    filenames = ["snapshot_{:03d}.hdf5".format(x) for x in range(n_snaps)]
+
+    if "--test" in sys.argv:
+        filenames = ['test_data.hdf5']
+
+    # We need more arguments than Pool.map allows
+    mapped_process = partial(processing_run, res=res, bbox_x=bbox_x, bbox_y=bbox_y)
+
+    with Pool(processes=n_cpus) as processing_pool:
+        result = processing_pool.map(mapped_process, filenames)
