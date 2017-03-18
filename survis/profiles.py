59e05478ec1ae9ab67faf24c7e67a0598b41477b
Josh Borrow <josh.borrow@gmail.com>
2017-03-18 16:52:00 +0000

    added scaffold for profile fitting

---
+++ survis/profiles.py
@@ -0,0 +1,19 @@
+""" This section of the code fits profiles to the disk and returns scale
+    lenghts, heights, radii... Whatever is relevant.
+
+    It is useful to compare these to the local Jeans' length to check if
+    the assumption of Schaye 2001 is valid. """
+
+
+def radial_profile(DG):
+    """ Takes the data grid for a galaxy and fits the profile radially.
+        Expects an exponential surface density profile."""
+    return 0
+
+
+def vertical_profile(DG):
+    return 0
+
+
+def local_jeans_length(DG):
+    return 0
